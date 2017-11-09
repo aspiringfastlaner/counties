d74dd7eb25f54fc36f0d4813b8ac698e5819205f
aspiringfastlaner <francis.chen89@gmail.com>
2017-11-08 20:26:41 -0800

    update

---
+++ ui.R
@@ -0,0 +1,39 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("USA Census Visualizations"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with information from the 2010 census"),
+      selectInput(input = "var",
+                  label = "Choose a Variable",
+                  choices = list("Percent White", "Percent Black",
+                                 "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
